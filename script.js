82c130e7aa3dbc53460f5efffe859c115916b421
Ravipaliwal1434 <138785779+Ravipaliwal1434@users.noreply.github.com>
2023-12-19 15:26:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,350 @@
+AOS.init({
+    offset: 120,
+    dealy: 1500,
+    duration:1500,
+    easing: 'ease'
+});
+
+
+// loader ///
+setTimeout(function () {
+  $('.loader_bg').fadeToggle();
+},5000);
+
+// mouse
+let myDiv = document.getElementById("mouse");
+
+//Detect touch device
+
+function isTouchDevice() {
+
+  try {
+
+    //We try to create TouchEvent. It would fail for desktops and throw error
+
+    document.createEvent("TouchEvent");
+
+    return true;
+
+  } catch (e) {
+
+    return false;
+
+  }
+
+}
+// mouse/////
+
+const move = (e) => {
+
+  //Try, catch to avoid any errors for touch screens (Error thrown when user doesn't move his finger)
+
+  try {
+
+    //PageX and PageY return the position of client's cursor from top left of screen
+
+    var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+
+    var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+
+  } catch (e) {}
+
+  //set left and top of div based on mouse position
+
+  myDiv.style.left = x - 30 + "px";
+
+  myDiv.style.top = y - 30 + "px";
+
+};
+
+//For mouse
+
+document.addEventListener("mousemove", (e) => {
+
+  move(e);
+
+});
+
+//For touch
+
+document.addEventListener("touchmove", (e) => {
+
+  move(e);
+
+});
+
+
+///////// choose Section ///////
+ 
+ $(document).ready(function(){
+     $('.counter-value').each(function(){
+         $(this).prop('Counter',0).animate({
+             Counter: $(this).text()
+         },{
+             duration: 3500,
+             easing: 'swing',
+             step: function (now){
+                 $(this).text(Math.ceil(now));
+             }
+         });
+     });
+ });
+
+// TEXT EFFECT ////////
+
+
+
+ const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["INSTRUCTOR.", "TRAINER.", "GOALS.", "LIFE."];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 1000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+
+
+
+
+// -----------portflio--------------
+$(document).ready( function() {
+
+	var itemSelector = '.grid-item'; 
+
+	var $container = $('#container').isotope({
+		itemSelector: itemSelector,
+		masonry: {
+		  columnWidth: itemSelector,
+		  isFitWidth: true
+		}
+	});
+
+	//Ascending order
+	var responsiveIsotope = [
+		[480, 7],
+		[720, 10]
+	];
+
+	var itemsPerPageDefault = 12;
+	var itemsPerPage = defineItemsPerPage();
+	var currentNumberPages = 1;
+	var currentPage = 1;
+	var currentFilter = '*';
+	var filterAtribute = 'data-filter';
+	var pageAtribute = 'data-page';
+	var pagerClass = 'isotope-pager';
+
+	function changeFilter(selector) {
+		$container.isotope({
+			filter: selector
+		});
+	}
+
+
+	function goToPage(n) {
+		currentPage = n;
+
+		var selector = itemSelector;
+			selector += ( currentFilter != '*' ) ? '['+filterAtribute+'="'+currentFilter+'"]' : '';
+			selector += '['+pageAtribute+'="'+currentPage+'"]';
+
+		changeFilter(selector);
+	}
+
+	function defineItemsPerPage() {
+		var pages = itemsPerPageDefault;
+
+		for( var i = 0; i < responsiveIsotope.length; i++ ) {
+			if( $(window).width() <= responsiveIsotope[i][0] ) {
+				pages = responsiveIsotope[i][1];
+				break;
+			}
+
+			
+
+		}
+
+		return pages;
+	}
+	
+	function setPagination() {
+
+		var SettingsPagesOnItems = function(){
+
+			var itemsLength = $container.children(itemSelector).length;
+			
+			var pages = Math.ceil(itemsLength / itemsPerPage);
+			var item = 1;
+			var page = 1;
+			var selector = itemSelector;
+				selector += ( currentFilter != '*' ) ? '['+filterAtribute+'="'+currentFilter+'"]' : '';
+			
+			$container.children(selector).each(function(){
+				if( item > itemsPerPage ) {
+					page++;
+					item = 1;
+				}
+				$(this).attr(pageAtribute, page);
+				item++;
+			});
+
+			currentNumberPages = page;
+
+		}();
+
+		var CreatePagers = function() {
+
+			var $isotopePager = ( $('.'+pagerClass).length == 0 ) ? $('<div class="'+pagerClass+'"></div>') : $('.'+pagerClass);
+
+			$isotopePager.html('');
+			
+			for( var i = 0; i < currentNumberPages; i++ ) {
+				var $pager = $('<a href="javascript:void(0);" class="pager" '+pageAtribute+'="'+(i+1)+'"></a>');
+					$pager.html(i+1);
+					
+					$pager.click(function(){
+						var page = $(this).eq(0).attr(pageAtribute);
+						goToPage(page);
+					});
+
+				$pager.appendTo($isotopePager);
+			}
+
+			$container.after($isotopePager);
+
+		}();
+
+	}
+
+	setPagination();
+	goToPage(1);
+
+	//Adicionando Event de Click para as categorias
+	$('.filters a').click(function(){
+		var filter = $(this).attr(filterAtribute);
+		currentFilter = filter;
+
+		setPagination();
+		goToPage(1);
+
+
+	});
+
+	//Evento Responsivo
+	$(window).resize(function(){
+		itemsPerPage = defineItemsPerPage();
+		setPagination();
+	});
+
+	
+
+});
+
+
+
+ $(document).ready( function() {   
+
+// filter items on button click
+$('.filter-button-group').on( 'click', 'li', function() {
+  var filterValue = $(this).attr('data-filter');
+  $('.grid').isotope({ filter: filterValue });
+  $('.filter-button-group li').removeClass('active');
+  $(this).addClass('active');
+});
+    })
+	
+
+ $(document).ready( function() {   
+
+// filter items on button click
+$('.isotope-pager').on( 'click', 'a', function() {
+  var filterValue = $(this).attr('data-page');
+
+  $('.isotope-pager a').removeClass('active');
+  $(this).addClass('active');
+});
+    })
+	
+	
+	
+$(document).ready(function(){
+$('.popupimg').magnificPopup({
+	type: 'image',
+  mainClass: 'mfp-with-zoom', 
+  gallery:{
+			enabled:true
+		},
+
+  zoom: {
+    enabled: true, 
+
+    duration: 300, // duration of the effect, in milliseconds
+    easing: 'ease-in-out', // CSS transition easing function
+
+    opener: function(openerElement) {
+
+      return openerElement.is('img') ? openerElement : openerElement.find('img');
+  }
+}
+
+});
+
+});
+
+
+// ========= BACK TO TOP ============
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+// counter
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
